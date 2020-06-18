c7f8140fcb2953875293884dc6a1de3924bfb718
Iryna Yavorska <yavorska-iryna@users.noreply.github.com>
2020-06-18 10:07:58 -0700

    updated
    predicts difficulty score
    plots route to visualize

--- notebooks/streamlit_webapp.py
+++ notebooks/streamlit_webapp.py
@@ -31,11 +31,12 @@ filename = file_selector()
 st.write('You selected `%s`' % filename)
 
 
-# In[ ]:
+# In[10]:
 
 
 # read proposed route
 import gpxpy
+
 gpx_file = open(filename, 'r')
 gpx = gpxpy.parse(gpx_file)
 
@@ -53,25 +54,81 @@ start = data[0]
 finish = data[-1]
 print(start)
 print(finish)
-df = pd.DataFrame(columns=['lon', 'lat', 'alt', 'time'])
+df = pd.DataFrame(columns=['lon', 'lat', 'alt'])
+
 for point in data:
-    df = df.append({'lon': point.longitude, 'lat' : point.latitude, 'alt' : point.elevation, 'time' : point.time}, ignore_index=True)
-df.describe()
+    df = df.append({'lon': point.longitude, 'lat' : point.latitude, 'alt' : point.elevation}, ignore_index=True)
+    
 
 
-# In[ ]:
+# In[44]:
+
+
+# get parameters from gpx
+import geopy.distance
+
+distance = [];
+elevation = [];
+for i in range(len(df)-1):
+    coords_1 = [df['lat'][i], df['lon'][i]]
+    coords_2 = [df['lat'][i+1], df['lon'][i+1]]
+    #print(coords_1, coords_2)
+    distance.append(geopy.distance.vincenty(coords_1, coords_2).miles)
+    elevation.append( df['alt'][i+1]-df['alt'][i])
+#print(distance)
+total_distance = np.sum(distance)
+
+elevation = np.array(elevation)
+gain = np.sum(elevation[np.where( elevation > 0 )])
+loss = np.sum(elevation[np.where( elevation < 0 )])
+gain_ft = gain*3.28084
+loss_ft = loss*3.28084
+
+longitude = df['lon'].mean()
+latitude = df['lat'].mean()
+high = df['alt'].max()
+low = df['alt'].min() 
+
+
+# In[48]:
+
+
+X_test = {'length' : total_distance, 'ascent' : gain_ft, 'descent' : loss_ft, 
+                        'high' : high, 'low' : low, 'longitude' : longitude, 'latitude' : latitude}
+
+
+# In[56]:
+
+
+#X_test = [total_distance, gain_ft, loss_ft, high, low, longitude, latitude]
+X = pd.DataFrame.from_dict(X_test, orient = 'index')
+
+
+# In[49]:
 
 
 #Load the trained ML model
+import pickle
 pkl_filename = "pickle_model.pkl"
-with open(pkl_filename, 'rb') as file:
-    
+with open(pkl_filename, 'rb') as file:   
     MLmodel = pickle.load(file)
+
+
+# In[62]:
+
+
+#Ingest user input into trained ML model
+y_predicted = MLmodel.predict(X.T)
+
+
+# In[63]:
+
+st.write('The difficulty of the route is {y}'.format(y = y_predicted))
+
 
 
 # In[ ]:
 
+st.map(df)
 
-#Ingest user input into trained ML model
-forest_predicted = MLmodel.predict(dfUserMLsub)
 
